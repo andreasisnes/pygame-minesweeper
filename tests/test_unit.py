6b4870b8799db7b4d8837fa56395982c041ccb8f
Martin Thoma <info@martin-thoma.de>
2020-03-17 10:56:37 +0100

    MAINT: Remove trailing spaces
    Add pre-commit to automatically do it, if the developer
    installs it
    
    Install with
        $ pre-commit install
    
    Run only once with
        $ pre-commit run --all-files

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -15,19 +15,19 @@ class TestUnit(unittest.TestCase):
         self.height = random.randint(10, 100)
         self.width  = random.randint(10, 100)
         self.mines  = random.randint(10, 90)
-        self.iter   = 100 
+        self.iter   = 100
         self.api = app.API(self.width, self.height, self.mines)
         self.assertEqual(self.width, self.api.width)
         self.assertEqual(self.mines, self.api.mines)
         self.assertEqual(self.height, self.api.height)
-        
+
 
     def test_game_reset(self):
         self.api.game_reset()
         self.assertEqual(self.width, self.api.width)
         self.assertEqual(self.mines, self.api.mines)
         self.assertEqual(self.height, self.api.height)
-    
+
     def test_game_new(self):
         self.height = random.randint(10, 100)
         self.width  = random.randint(10, 100)
@@ -40,11 +40,11 @@ class TestUnit(unittest.TestCase):
     def test_board_dim(self):
         self.assertEqual(len(self.api.board), self.api.height)
         self.assertEqual(len(self.api.board[0]), self.api.width)
-    
+
     def test_sheet_dim(self):
         self.assertEqual(len(self.api._sheet), self.api.height)
         self.assertEqual(len(self.api._sheet[0]), self.api.width)
-    
+
     def test_tile_open(self):
         for _ in range(self.iter):
             self.height = random.randint(10, 100)
@@ -66,7 +66,7 @@ class TestUnit(unittest.TestCase):
                 for j in [x+1, x-1]:
                     if self.api.tile_valid(i, j):
                         self.assertNotEqual(self.api.board[i][j], self.api.encoder.num_mine)
-    
+
     def test_playing(self):
         for _ in range(self.iter):
             self.height = random.randint(10, 100)
@@ -100,7 +100,7 @@ class TestUnit(unittest.TestCase):
                     if not self.api.encoder.is_mine(self.api.sheet[y][x]):
                         self.api.tile_open(y, x)
             self.assertTrue(self.api.is_game_done)
-    
+
 
 class TestEncoder(unittest.TestCase):
     def setUp(self):
@@ -113,7 +113,7 @@ class TestEncoder(unittest.TestCase):
             self.assertEqual(val, self.encoder.ascii2num(key))
             with self.assertRaises(KeyError):
                 self.encoder.ascii2num(key + "test")
-    
+
     def test_num2ascii(self):
         for key, val in self.encoder._num2ascii.items():
             self.assertEqual(key, self.encoder._ascii2num[val])
@@ -127,18 +127,18 @@ class TestEncoder(unittest.TestCase):
     def test_getitem_ascii(self):
         for key, val in self.encoder._ascii2num.items():
             self.assertEqual(val, self.encoder[key])
-    
+
     def test_getitem_num(self):
         for key, val in self.encoder._num2ascii.items():
             self.assertEqual(val, self.encoder[key])
-    
+
     def test_is_tile_ascii(self):
         for key in self.encoder._ascii2num.keys():
             if key == self.encoder.ascii_tile:
                 self.assertEqual(True, self.encoder.is_tile(key))
             else:
                 self.assertEqual(False, self.encoder.is_tile(key))
-    
+
     def test_is_tile_num(self):
         for key in self.encoder._num2ascii.keys():
             if key == self.encoder.num_tile:
@@ -152,13 +152,13 @@ class TestEncoder(unittest.TestCase):
                 self.assertEqual(True, self.encoder.is_mine(key))
             else:
                 self.assertEqual(False, self.encoder.is_mine(key))
-    
+
     def test_is_min_num(self):
         for key in self.encoder._num2ascii.keys():
             if key == self.encoder.num_mine:
                 self.assertEqual(True, self.encoder.is_mine(key))
             else:
                 self.assertEqual(False, self.encoder.is_mine(key))
-    
+
 if __name__ == '__main__':
     unittest.main()
